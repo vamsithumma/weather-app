c3b669e3954ab8a41bee52c7a0eb9bb0cf97432f
Vamsi thumma <vamsithumma2001@gmail.com>
2023-09-01 18:36:56 +0530

    Handled Wrong Location Exception

--- src/components/WeatherApp/WeatherApp.jsx
+++ src/components/WeatherApp/WeatherApp.jsx
@@ -21,21 +21,24 @@ const WaeatherApp = ()=> {
         }
         
         let url = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=Metric&appid=${api_key}`;
-        let response = await fetch(url);
-        let data = await response.json();
 
         const humidity = document.getElementsByClassName("humidity-percent");
         const temperature = document.getElementsByClassName("weather-temp");
         const wind= document.getElementsByClassName("wind-rate");
         const location= document.getElementsByClassName("weather-location");
 
-        humidity[0].innerHTML = data.main.humidity+" %";
-        wind[0].innerHTML = data.wind.speed+ " km/h";
-        temperature[0].innerHTML = data.main.temp+ " °c";
-        location[0].innerHTML= data.name;
-        console.log(data);
-
-    }
+        try{
+            let response = await fetch(url);
+            let data = await response.json();
+            humidity[0].innerHTML = data.main.humidity+" %";
+            wind[0].innerHTML = data.wind.speed+ " km/h";
+            temperature[0].innerHTML = data.main.temp+ " °c";
+            location[0].innerHTML= data.name;
+        }catch(err){
+            alert("Wrong Location Entered - Please enter correct location");
+        }
+        
+     }
   return (
     <div className='container'>
         <div className="top-bar">
